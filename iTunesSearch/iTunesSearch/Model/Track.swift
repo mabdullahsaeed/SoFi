363312811484ed47cb01e69ef177c10938daa3f3
Abdullah Saeed <abdullahsaeed@Abdullahs-MacBook-Pro.local>
2019-07-10 06:30:12 -0700

    iTunes Project

---
+++ iTunesSearch/iTunesSearch/Model/Track.swift
@@ -0,0 +1,32 @@
+
+import Foundation.NSURL
+
+//
+// MARK: - Track
+//
+
+/// Query service creates Track objects
+class Track {
+  //
+  // MARK: - Constants
+  //
+  let artist: String
+  let index: Int
+  let name: String
+  let previewURL: URL
+  
+  //
+  // MARK: - Variables And Properties
+  //
+  var downloaded = false
+  
+  //
+  // MARK: - Initialization
+  //
+  init(name: String, artist: String, previewURL: URL, index: Int) {
+    self.name = name
+    self.artist = artist
+    self.previewURL = previewURL
+    self.index = index
+  }
+}
